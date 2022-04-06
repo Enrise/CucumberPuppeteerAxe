a9a2e383ae275c378ad539095479547bfcd9bafa
Henk-Jan van Voorthuijsen <hjvvoorthuijsen@enrise.com>
2022-04-06 10:10:05 +0200

    Wrong import

--- features/step_definitions/accessibilityChecks.js
+++ features/step_definitions/accessibilityChecks.js
@@ -1,4 +1,4 @@
-const { Then, Given } = require('@axe-core/pupeeteer');
+const { Then, Given } = require('cucumber');
 const { accessibilityCheck } = require('../support/accessibilityCheck');
 
 Given('I disable the accessibility rule(s) {string}', function(rule) {
